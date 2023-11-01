a071add7d2fdf9bdff0e1648d02a5ead8924d47d
nick_hart <nhartmusic@gmail.com>
2023-11-01 14:40:17 -0700

    connected to firebase

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log('free palestine')
- 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAdl5SGgE5y_UY5KnASfM0ch2z2-uEzNI",
+    authDomain: "fir-9-test-dcbed.firebaseapp.com",
+    projectId: "fir-9-test-dcbed",
+    storageBucket: "fir-9-test-dcbed.appspot.com",
+    messagingSenderId: "437638549132",
+    appId: "1:437638549132:web:b900350f78b6747306f5a9"
+  }
+
+  initializeApp(firebaseConfig)
+  
